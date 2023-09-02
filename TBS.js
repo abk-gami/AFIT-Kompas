3ac828d0f0be26583ed6841d6379d5a073d923ae
abk-gami <yahabubakar01@gmail.com>
2023-09-02 19:38:17 +0100

    chelsea

--- TBS.js
+++ TBS.js
@@ -14,25 +14,26 @@ import {
   BottomSheetModalProvider,
 } from "@gorhom/bottom-sheet";
 import { useRef, useState } from "react";
-import { AntDesign } from "@expo/vector-icons";
-import { Entypo } from "@expo/vector-icons";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function BottomSheet() {
-  const [darkmode, setDarkmode] = useState(false);
-  const [device, setDevice] = useState(false);
-  const { width } = useWindowDimensions();
-  const [theme, setTheme] = useState("dim");
   const [isOpen, setIsOpen] = useState(false);
 
   const bottomSheetModalRef = useRef(null);
 
-  const snapPoints = ["25%", "48%", "75%"];
+  const snapPoints = [ "25%", "50%", "98%"];
 
   function handlePresentModal() {
     bottomSheetModalRef.current?.present();
     setTimeout(() => {
       setIsOpen(true);
+    }, 100);
+  }
+  function closeIt() {
+    bottomSheetModalRef.current?.present();
+    setTimeout(() => {
+      setIsOpen(false);
+      bottomSheetModalRef.current?.dismiss();
     }, 100);
   }
 
@@ -42,65 +43,23 @@ export default function BottomSheet() {
         <View
           style={[
             styles.container,
-            { backgroundColor: isOpen ? "#b4b4b470" : "white" },
+            { backgroundColor: isOpen ? "#c6aaaa" : "#ff0000b5" },
           ]}
         >
           <Button title="Present Modal" onPress={handlePresentModal} />
-          {/* <StatusBar style="auto" /> */}
           <BottomSheetModal
             ref={bottomSheetModalRef}
             index={1}
             snapPoints={snapPoints}
-            backgroundStyle={{ borderRadius: 50 }}
+            backgroundStyle={{ borderRadius: 30, backgroundColor: '#4e2a2a' }}
             onDismiss={() => setIsOpen(false)}
+            enablePanDownToClose={true}
           >
             <View style={styles.contentContainer}>
-              <Text style={[styles.title, { marginBottom: 20 }]}>
-                Dark mode
-              </Text>
-              <View style={styles.row}>
-                <Text style={styles.subtitle}>Dark mode</Text>
-                <Switch
-                  value={darkmode}
-                  onChange={() => setDarkmode(!darkmode)}
-                />
-              </View>
-              <View style={styles.row}>
-                <Text style={styles.subtitle}>Use device settings</Text>
-                <Switch value={device} onChange={() => setDevice(!device)} />
-              </View>
-              <Text style={styles.description}>
-                Set Dark mode to use the Light or Dark selection located in your
-                device Display and Brightness settings.
-              </Text>
-              <View
-                style={{
-                  width: width,
-                  borderBottomWidth: StyleSheet.hairlineWidth,
-                  borderBottomColor: "gray",
-                  marginVertical: 30,
-                }}
-              />
-              <Text style={[styles.title, { width: "100%" }]}>Theme</Text>
-              <Pressable style={styles.row} onPress={() => setTheme("dim")}>
-                <Text style={styles.subtitle}>Dim</Text>
-                {theme === "dim" ? (
-                  <AntDesign name="checkcircle" size={24} color="#4A98E9" />
-                ) : (
-                  <Entypo name="circle" size={24} color="#56636F" />
-                )}
-              </Pressable>
-              <Pressable
-                style={styles.row}
-                onPress={() => setTheme("lightsOut")}
-              >
-                <Text style={styles.subtitle}>Lights out</Text>
-                {theme === "lightsOut" ? (
-                  <AntDesign name="checkcircle" size={24} color="#4A98E9" />
-                ) : (
-                  <Entypo name="circle" size={24} color="#56636F" />
-                )}
-              </Pressable>
+            <Text>Hello</Text>
+          <Button 
+          title="CLose" onPress={closeIt}
+          />
             </View>
           </BottomSheetModal>
         </View>
