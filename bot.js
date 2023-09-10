680a829e2df12ffe2b76da7a3dec76cd9e9fb8bb
abk-gami <yahabubakar01@gmail.com>
2023-09-10 21:54:13 +0100

    h

--- bot.js
+++ bot.js
@@ -8,70 +8,66 @@ import {
   BottomSheet,
   Text,
 } from 'react-native';
+import BottomSheet from 'react-native-bottom-sheet';
 
-const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [screenToShow, setScreenToShow] = useState(null);
-
-  const buttons = [
-    {
-      title: 'Screen 1',
-      component: Screen1,
-    },
-    {
-      title: 'Screen 2',
-      component: Screen2,
-    },
-    {
-      title: 'Screen 3',
-      component: Screen3,
-    },
-  ];
-
-  const handleButtonPress = (button) => {
-    setScreenToShow(button.component);
-    setIsOpen(true);
-  };
-
-  const renderBottomSheet = () => {
-    if (!screenToShow) {
-      return null;
-    }
-
-    return (
-      <BottomSheet
-        isVisible={isOpen}
-        onDismiss={() => setIsOpen(false)}
-      >
-        <View>
-          {screenToShow}
-        </View>
-      </BottomSheet>
-    );
-  };
-
+const Screen1 = () => {
   return (
     <View>
-      {buttons.map((button, index) => (
-        <Button
-          key={index}
-          title={button.title}
-          onPress={handleButtonPress}
-        />
-      ))}
-      {renderBottomSheet()}
+      <Text>This is Screen 1</Text>
     </View>
   );
 };
 
-const Screen1 = () => <Text>This is Screen 1</Text>;
-const Screen2 = () => <Text>This is Screen 2</Text>;
-const Screen3 = () => <Text>This is Screen 3</Text>;
+const Screen2 = () => {
+  return (
+    <View>
+      <Text>This is Screen 2</Text>
+    </View>
+  );
+};
+
+const Screen3 = () => {
+  return (
+    <View>
+      <Text>This is Screen 3</Text>
+    </View>
+  );
+};
+
+const App = () => {
+
+  const openBottomSheet = (screen) => {
+    const bottomSheet = new BottomSheet();
+    bottomSheet.open({
+      screen: screen,
+    });
+  };
+
+  return (
+    <View>
+      <Button onPress={() => {
+        openBottomSheet(Screen1);
+      }}
+      title='Screen 1'
+      />
+        
+      
+      <Button onPress={() => {
+        openBottomSheet(Screen2);
+      }}
+      title='screen 2'
+      />
+      <Button onPress={() => {
+        openBottomSheet(Screen3);
+      }}
+      title='screen 3'
+      />
+  
+    </View>
+  );
+};
+
+
 
 export default App;
 
-// In this code, we have an array of buttons, each of which represents a different screen. When a button is pressed, the `handleButtonPress` function is called, which sets the `screenToShow` state to the component that corresponds to the button that was pressed. The `renderBottomSheet` function then renders the bottom sheet, which contains the `screenToShow` component.
-
-// The bottom sheet is closed when the user clicks outside of it or presses the back button.
-
-// I hope this helps! Let me know if you have any other questions.
