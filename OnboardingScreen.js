9c3567506b407d384760255393f0a5ab7399c56e
abk-gami <yahabubakar01@gmail.com>
2023-10-16 23:19:53 +0100

    r

--- OnboardingScreen.js
+++ OnboardingScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, Button } from 'react-native'
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native'
 import React from 'react'
 import Onboarding from 'react-native-onboarding-swiper';
 
@@ -13,43 +13,43 @@ const OnboardingScreen = ({navigation}) => {
       backgroundColor: '#ef340f',
       image: <Image source={require('./assets/logo.png')} style={styles.image} />,
       title: 'Please TURN ON LOCATION for better Experience',
-      subtitle: 'Made for AFIT students..By an AFIT student!',
+      subtitle: 'Made for AFIT students...And AFIT Explorers!',
     },
     {
       backgroundColor: 'orange',
       image: <Image source={require('./assets/1.png')} style={styles.image} />,
-      title: 'A map that enables ease of movement around AFIT',
-      subtitle: '....',
+      title: 'An app that enables ease of movement around AFIT',
+      subtitle: 'With a map that aides navigation',
     },
     {
       backgroundColor: 'yellow',
       image: <Image source={require('./assets/2.png')} style={styles.image} />,
       title: 'Scroll through your favorite landmarks',
-      subtitle: '....',
+      subtitle: 'You get to view the whole AFIT Environment!',
     },
     {
       backgroundColor: 'green',
-      image: <Image source={require('./assets/3.png')} style={styles.image} />,
-      title: 'Search through a list of places in AFIT',
-      subtitle: '....',
+      image: <Image source={require('./assets/4.png')} style={styles.image} />,
+      title: 'Get directional guides on happening events',
+      subtitle: 'Where is it happening? and how can I get there?',
     },
     {
       backgroundColor: '#001b7c',
-      image: <Image source={require('./assets/4.png')} style={styles.image} />,
-      title: 'See your current location',
-      subtitle: '....',
+      image: <Image source={require('./assets/3.png')} style={styles.image} />,
+      title: 'Search through a list of places in AFIT',
+      subtitle: 'And tap the location to get directions to where you are going',
     },
     {
       backgroundColor: 'indigo',
       image: <Image source={require('./assets/5.png')} style={styles.image} />,
       title: 'Get directions from where you are to where you are going ',
-      subtitle: '....',
+      subtitle: 'Are you Lost? Find Your Way',
     },
     {
       backgroundColor: 'violet',
       image: <Image source={require('./assets/6.png')} style={styles.image} />,
-      title: 'Get news about upcoming events in AFIT',
-      subtitle: '....',
+      title: 'Get news about happening events in AFIT',
+      subtitle: 'To always keep you updated with verified news',
     },
    
     
